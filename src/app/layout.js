8e030cf1e46357211deb84e551400f37d3a299ce
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-01 18:54:28 +0900

    main-component-commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@/assets/style.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ src/utils/lenis.js
@@ -0,0 +1,5 @@
+const lenis = () => {
+  console.log(lenis);
+};
+
+export default lenis;
